52160d944ad8ce62280c2dbcf7ff4d421a88e82a
高彬 <gaobin2496@gmail.com>
2020-05-06 15:49:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,4 +8,5 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+echo "src-git openclash https://github.com/vernesong/OpenClash" >> feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
