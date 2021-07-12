0274e22189103366cafdfee6f83b9c4378909264
gao <gaobin2496@gmail.com>
2021-07-12 11:29:37 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,7 +6,6 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 echo "src-git openclash https://github.com/vernesong/OpenClash" >> feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
+
