cd50b88389c33aa56ed239c0ce4c5a7f1029b794
高彬 <gaobin2496@gmail.com>
2021-12-27 19:16:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,6 +6,6 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+# echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 
